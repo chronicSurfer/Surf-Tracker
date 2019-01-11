74799eca891addc49142b9ad33116d6df7b3588d
chronicSurfer <kyle.umbarger@gmail.com>
2019-01-10 23:58:08 -0800

    Basic node setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 //this version of the surf tracker is going to be done in node, express, mongo, and mongoose
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=>{
+    console.log('Server running on PORT:', PORT);
+});
+
